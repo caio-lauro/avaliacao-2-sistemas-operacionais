86f0a4dd38f62d5f603740481f3f229c8cfac3c8
caio-lauro <caiolaurodelima@gmail.com>
2025-10-30 15:28:44 -0300

    mover declaração de cores para evitar erros de múltipla definição

---
+++ src/variaveis.c
@@ -0,0 +1,10 @@
+#include "variaveis.h"
+
+// Definir cores de fundo
+int fundo[3] = {2, 48, 32};
+int fundo_borda[3] = {72, 60, 50};
+
+// Definir cores de texto
+int texto_colunas[3] = {200, 200, 200};
+int texto_linhas[3] = {50, 50, 200};
+int texto[3] = {200, 50, 120};
