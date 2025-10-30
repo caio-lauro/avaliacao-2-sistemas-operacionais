f7879fe19bbe559786f7f45856a238bbde047fac
caio-lauro <caiolaurodelima@gmail.com>
2025-10-30 15:31:37 -0300

    simplificação do código da main ao usar os utilitários

--- src/main.c
+++ src/main.c
@@ -1,86 +1,15 @@
 #include "printcolorido.h"
 #include "variaveis.h"
-
-void imprimir_matriz();
+#include "utils.h"
 
 int main() {
-    imprimir_matriz();
+    // Criar matriz com um tamanho específico
+    matriz_t *matriz = criar_matriz(TAMANHO);
+    imprimir_matriz(matriz);
+
+    // TODO: Lógica
+
+    liberar_matriz(&matriz);
 
     return 0;
-}
-
-void imprimir_matriz() {
-    // Definir cores de fundo
-    int fundo[3] = RGB_FUNDO;
-    int fundo_borda[3] = {80, 80, 80};
-
-    // Definir cores de texto
-    int texto_colunas[3] = {200, 200, 200};
-    int texto_linhas[3] = {50, 50, 255};
-    int texto[3] = {100, 0, 200};
-
-    // Variável para armazenar números das guias com espaçamento
-    char numero_guia[5];
-
-    // Variável para armazenar o tamanho da matriz de impressão
-    const int TAMANHO_MATRIZ_IMPRESSAO = 3 * TAMANHO + 3;
-
-    // Variável para armazenar o tamanho da borda
-    const int TAMANHO_BORDA = TAMANHO_MATRIZ_IMPRESSAO + 2;
-
-    // Borda superior
-    for (int j = 0; j < TAMANHO_BORDA; j++) {
-        print_colorido_rgb(" ", fundo_borda, NULL);
-    }
-    printf("\n");
-
-    // Borda lateral da esquerda
-    print_colorido_rgb(" ", fundo_borda, NULL);
-
-    // Imprimir guias de colunas
-    // Espaçamento inicial
-    print_colorido_rgb("  ", fundo, texto_colunas);
-
-    // Números das colunas
-    for (int j = 0; j < TAMANHO; j++) {
-        sprintf(numero_guia, " %2i", j);
-        print_colorido_rgb(numero_guia, fundo, texto_colunas);
-    }
-    print_colorido_rgb(" ", fundo, texto_colunas);
-
-    // Borda lateral da direita
-    print_colorido_rgb(" ", fundo_borda, NULL);
-
-    printf("\n");
-
-    // Imprimir linhas da matriz
-    for (int i = 0; i < TAMANHO; i++) {
-        // Borda lateral da esquerda
-        print_colorido_rgb(" ", fundo_borda, NULL);
-
-        // Imprimir guia de linhas
-        sprintf(numero_guia, "%2i ", i);
-        print_colorido_rgb(numero_guia, fundo, texto_linhas);
-
-        // Imprimir elementos da linha
-        for (int j = 0; j < TAMANHO; j++) {
-            if (i >= 1 && j >= 1 && ((i - 1) % 3 == 0) && ((j - 1) % 3 == 0)) {
-                // TODO: criar thread
-                print_colorido_rgb(" T ", fundo, texto);
-            } else {
-                print_colorido_rgb(" - ", fundo, texto);
-            }
-        }
-
-        // Borda lateral da direita
-        print_colorido_rgb(" ", fundo_borda, NULL);
-
-        printf("\n");
-    }
-
-    // Borda inferior
-    for (int j = 0; j < TAMANHO_BORDA; j++) {
-        print_colorido_rgb(" ", fundo_borda, NULL);
-    }
-    printf("\n");
 }
