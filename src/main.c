07ee7e1f2271c47f7940bdc8a4ec6f19f35e277e
caio-lauro <caiolaurodelima@gmail.com>
2025-11-11 10:54:47 -0300

    inclusão de threads.h e adição da lógica

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "printcolorido.h"
 #include "variaveis.h"
 #include "utils.h"
+#include "threads.h"
 
 int main() {
     // Tamanho da matriz
@@ -21,7 +22,37 @@ int main() {
 
     imprimir_matriz(matriz);
 
-    // TODO: Lógica
+    // Thread IDs
+    pthread_t thread_central, thread_incendiaria;
+
+    // Criação das threads sensores
+    for (size_t i = 0; i < threads.size; i++) {
+        pthread_t thread_sensor;
+        sensor_args_t sensor_args;
+        sensor_args.matriz = &matriz;
+        sensor_args.sensor = threads.elementos[i];
+        pthread_create(&thread_sensor, NULL, sensor, (void*)&sensor_args);
+        threads.elementos[i].thread_id = thread_sensor;
+    }
+
+    // Criação da thread incendiária
+    incendiaria_args_t incendiaria_args;
+    incendiaria_args.matriz = &matriz;
+    incendiaria_args.arr = &threads;
+    pthread_create(&thread_incendiaria, NULL, incendiaria, (void*)&incendiaria_args);
+
+    // Criação da thread central
+    central_args_t central_args;
+    central_args.matriz = &matriz;
+    pthread_create(&thread_central, NULL, central, (void*)&central_args);
+
+    // Impressão da matriz
+    while (true) {
+        sleep(1);
+        imprimir_matriz(matriz);
+    }
+
+    pthread_exit(NULL);
 
     liberar_matriz(&matriz);
     liberar_vetor(&threads);
