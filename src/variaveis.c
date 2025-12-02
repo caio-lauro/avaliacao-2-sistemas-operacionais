6aa26caa4c5b71d606cf2f7d52687d962338a754
caio-lauro <caiolaurodelima@gmail.com>
2025-12-02 14:19:22 -0300

    mudança de cores

--- src/variaveis.c
+++ src/variaveis.c
@@ -1,10 +1,10 @@
 #include "variaveis.h"
 
 // Definir cores de fundo
-int fundo[3] = {2, 48, 32};
-int fundo_borda[3] = {72, 60, 50};
+int fundo[3] = {101, 179, 7};
+int fundo_borda[3] = {99, 79, 70};
 
 // Definir cores de texto
-int texto_colunas[3] = {200, 200, 200};
-int texto_linhas[3] = {50, 50, 200};
-int texto[3] = {200, 50, 120};
+int texto_colunas[3] = {240, 240, 240};
+int texto_linhas[3] = {242, 241, 239};
+int texto[3] = {10, 10, 10};
